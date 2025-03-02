f793693084b7e7cd0cb3c34c1b06fce4e74cd3d1
Michel <fistonkajibwami2@gmail.com>
2025-03-02 00:17:07 -0330

    add new update

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenunavigationDirective } from '../menunavigation.directive';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
     selector: 'app-service',
@@ -10,6 +14,24 @@ import { MenunavigationDirective } from '../menunavigation.directive';
     styleUrl: './service.component.css'
 })
 export class ServiceComponent {
+  @ViewChildren('animatedBox') animatedBoxes!: QueryList<ElementRef>;
+  ngAfterViewInit() {
+    this.animatedBoxes.forEach((box, index) => {
+      gsap.from(box.nativeElement, {
+        opacity: 0,
+        y: 100,
+        scale: 0.9,
+        duration: 1.2,
+        ease: "power3.out",
+        delay: index * 0.2, // Décalage progressif des animations
+        scrollTrigger: {
+        trigger: box.nativeElement,
+        start: "top 85%", // Déclenchement quand l’élément est visible
+        toggleActions: "play none none none",
+        },
+      });
+    });
+  }
  
 }
 
