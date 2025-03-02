e5e27c3600dcc44943962105b80f1b84d90a484b
Michel <fistonkajibwami2@gmail.com>
2025-03-02 01:41:22 -0330

    add new update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from '../form/form.component';
+import gsap from 'gsap';
 
 
 @Component({
@@ -11,5 +12,22 @@ import { FormComponent } from '../form/form.component';
     styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  @ViewChildren('animatedBox') animatedBoxes!: QueryList<ElementRef>;
+  ngAfterViewInit() {
+    this.animatedBoxes.forEach((box, index) => {
+      gsap.from(box.nativeElement, {
+        opacity: 0,
+        y: 100,
+        scale: 0.9,
+        duration: 1.2,
+        ease: "power3.out",
+        delay: index * 0.2, // Décalage progressif des animations
+        scrollTrigger: {
+        trigger: box.nativeElement,
+        start: "top 85%", // Déclenchement quand l’élément est visible
+        toggleActions: "play none none none",
+        },
+      });
+    });
+  }
 }
